445655c60ecb4d81d7cfbea0c12ea75229eed9da
Aleksandr Savchenko <savchenko.arts@gmail.com>
2016-02-20 21:27:40 +0200

    seventh commit

--- src/ua/savchenko/calculatorgui/service/Calculator.java
+++ src/ua/savchenko/calculatorgui/service/Calculator.java
@@ -20,14 +20,6 @@ public class Calculator {
                 result = x;
                 break;
         }
-<<<<<<< HEAD
         return result;
-=======
-
-        frame.getTextField().setText("" + frame.getResult());
-        if(!frame.getLastCommand().equals("=")){
-            frame.getTextArea().setText(frame.getTextArea().getText() + " " + resultFirst + " " + frame.getLastCommand() + " " + x + " = " + frame.getResult() + "\n");
-        }
->>>>>>> 4e788b5ba7c5273bc391b62544cda9141cd11c51
     }
 }
